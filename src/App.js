4f7637969663cefda97e52ab35c30d00e6ffbcb8
ildar-khasanov <ildar@mail.ru>
2025-02-12 23:07:36 +0500

    добавлена ссылка для корректного маршрута

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     return (
         <>
             <Provider store={store}>
-                <Router>
+                <Router basename="/portfolio-frontend">
                     <ScrollToTop />
                     <NavBar />
 
